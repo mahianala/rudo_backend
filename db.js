b26f3811e0709210aaae5845afe3db0e813cbfdd
RohitIkoparde <rohitk@emproto.com>
2023-02-06 23:33:31 +0530

    final commit

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host : 'localhost',
     user : 'root',
-    password : 'naga@123',
+    password : 'root',
     waitForConnections : true,
     connectionLimit : 10,
     database : 'rudo'
